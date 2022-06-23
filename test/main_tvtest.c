8bb0e40d151a9374615e3503cbc12084f868cadb
Nick Xie <nick@khadas.com>
2022-06-23 17:34:32 +0800

    change hdmi in source for VIM4
    Signed-off-by: Nick Xie <nick@khadas.com>

--- test/main_tvtest.c
+++ test/main_tvtest.c
@@ -6,8 +6,11 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <signal.h>
 #include <TvClientWrapper.h>
 #include <CTvClientLog.h>
+
+static int run = 1;
 
 static int WriteSysfs(const char *path, const char *cmd)
 {
@@ -25,7 +28,7 @@ static int WriteSysfs(const char *path, const char *cmd)
 
 static int DisplayInit()
 {
-    WriteSysfs("/sys/class/graphics/fb0/osd_display_debug", "1");
+//    WriteSysfs("/sys/class/graphics/fb0/osd_display_debug", "1");
     WriteSysfs("/sys/class/graphics/fb0/blank", "1");
     return 0;
 
@@ -50,89 +53,34 @@ static void TvEventCallback(event_type_t eventType, void *eventData)
     }
 }
 
+static void signal_handler(int s)
+{
+    void *retval;
+    run = 0;
+
+	WriteSysfs("/sys/class/graphics/fb0/blank", "0");
+    signal(s, SIG_DFL);
+    raise(s);
+}
+
 int main(int argc, char **argv) {
     struct TvClientWrapper_t * pTvClientWrapper = GetInstance();
     setTvEventCallback(TvEventCallback);
-    char Command[1];
-    int run = 1;
-    tv_source_input_t CurrentSource = SOURCE_HDMI1;
+    tv_source_input_t CurrentSource = SOURCE_HDMI2;
+	StopTv(pTvClientWrapper, CurrentSource);
+
+	sleep(1);
+
     DisplayInit();
 
-    printf("#### please select cmd####\n");
-    printf("#### select 1 to hdmi 1 ####\n");
-    printf("#### select 2 to hdmi 2 ####\n");
-    printf("#### select 3 to hdmi 3 ####\n");
-    printf("#### select 4 to edid 1.4 (this is default) ####\n");
-    printf("#### select 5 to edid 2.0 ####\n");
-    printf("#### select 6 to edid auto ####\n");
-    printf("#### select 7 to AV1 ####\n");
-    printf("#### select 8 to AV2 ####\n");
-    printf("#### select q to stop####\n");
-    printf("##########################\n");
-    while (run) {
-        scanf("%s", Command);
-        switch (Command[0]) {
-          case 'q': {
-            StopTv(pTvClientWrapper, CurrentSource);
-            WriteSysfs("/sys/class/graphics/fb0/blank", "0");
-            ReleaseInstance(&pTvClientWrapper);
-            run = 0;
-            break;
-          }
-          case '1': {
-              StopTv(pTvClientWrapper, CurrentSource);
-              CurrentSource = SOURCE_HDMI1;
-              WriteSysfs("/sys/class/graphics/fb0/blank", "0");
-              StartTv(pTvClientWrapper, CurrentSource);
-              break;
-          }
-         case '2': {
-              StopTv(pTvClientWrapper, CurrentSource);
-              CurrentSource = SOURCE_HDMI2;
-              WriteSysfs("/sys/class/graphics/fb0/blank", "0");
-              StartTv(pTvClientWrapper, CurrentSource);
-              break;
-          }
-          case '3': {
-              StopTv(pTvClientWrapper, CurrentSource);
-              CurrentSource = SOURCE_HDMI3;
-              WriteSysfs("/sys/class/graphics/fb0/blank", "0");
-              StartTv(pTvClientWrapper, CurrentSource);
-              break;
-          }
-          case '4': {
-              SetEdidVersion(pTvClientWrapper, CurrentSource, 0);
-              break;
-          }
-          case '5': {
-              SetEdidVersion(pTvClientWrapper, CurrentSource, 1);
-              break;
-          }
-          case '6': {
-              SetEdidVersion(pTvClientWrapper, CurrentSource, 2);
-              break;
-          }
-          case '7': {
-              StopTv(pTvClientWrapper, CurrentSource);
-              CurrentSource = SOURCE_AV1;
-              WriteSysfs("/sys/class/graphics/fb0/blank", "0");
-              StartTv(pTvClientWrapper, CurrentSource);
-              break;
-          }
-         case '8': {
-              StopTv(pTvClientWrapper, CurrentSource);
-              CurrentSource = SOURCE_AV2;
-              WriteSysfs("/sys/class/graphics/fb0/blank", "0");
-              StartTv(pTvClientWrapper, CurrentSource);
-              break;
-          }
-          default: {
-              LOGD("invalid cmd.\n");
-              break;
-          }
-        }
-        fflush (stdout);
-    }
+    StartTv(pTvClientWrapper, CurrentSource);
+
+	signal(SIGINT, signal_handler);
+
+	while (run)
+	{
+		sleep(1);
+	}
 
     return 0;
 }
